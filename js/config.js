fd1b19640b4fee591cab9aa34e76bf09c1b1f21c
nickeyspero <97686114+nickeyspero@users.noreply.github.com>
2023-04-22 17:40:06 -0500

    new_config

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId:         '',         // e.g. us-east-2_uXboG5pAb
-        userPoolClientId:   '',         // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region:             ''          // e.g. us-east-2
+        userPoolId:         'us-east-1_aOCNcGzbZ',         // e.g. us-east-1_uXboG5pAb
+        userPoolClientId:   '75mbpq6qu722pm43e24oh6g5lr',         // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region:             'us-east-1'          // e.g. us-east-1
     },
     api: {
         invokeUrl:          ''          // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
